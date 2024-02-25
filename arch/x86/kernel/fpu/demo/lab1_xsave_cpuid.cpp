75dd3ffc7444587d27bb961ae1c0dff63ef4c0bc
上田光華 <masswie@foxmail.com>
2024-02-25 17:49:52 +0800

    state compoment

--- arch/x86/kernel/fpu/demo/lab1_xsave_cpuid.cpp
+++ arch/x86/kernel/fpu/demo/lab1_xsave_cpuid.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+/*
+
+    Linux Kernel: 
+        https://elixir.bootlin.com/linux/v6.8-rc5/source/arch/x86/kernel/fpu/xstate.c#L230
+        https://elixir.bootlin.com/linux/v6.8-rc5/source/arch/x86/include/asm/fpu/xstate.h#L15
+
+    LLVM: 
+        https://github.com/llvm/llvm-project/blob/release/13.x/llvm/lib/Support/Host.cpp#L487
+
+*/
+
+
 #define XSTATE_CPUID		0x0000000d
 
 /// __cpuIdCount - Execute the specified cpuid and return the 4 values in
@@ -26,11 +38,39 @@ int main(){
     std::cout << " Hello \n"; 
 
     unsigned eax, ebx, ecx, edx;
-    __cpuIdCount(XSTATE_CPUID, 2,  &eax, &ebx, &ecx, &edx); 
 
+    // State Component 2 is AVX, read Intel SDM 13.5.3 AVX State
+    // its Offset is 576 and size is 256. 
+    __cpuIdCount(XSTATE_CPUID, 2,  &eax, &ebx, &ecx, &edx); 
     std::cout << "State Component 2:  \n"; 
-    std::cout << "flag: " << ecx <<  "\n"; 
-    std::cout << "offset: " << ebx << "\n"; 
-    std::cout << "size: " << eax << "\n"; 
+    std::cout << "\tflag: " << ecx <<  "\n"; 
+    std::cout << "\toffset: " << ebx << "\n"; 
+    std::cout << "\tsize: " << eax << "\n"; 
+
+
+    // State Component 3 is MPX
+    __cpuIdCount(XSTATE_CPUID, 3,  &eax, &ebx, &ecx, &edx); 
+    std::cout << "State Component 3:  \n"; 
+    std::cout << "\tflag: " << ecx <<  "\n"; 
+    std::cout << "\toffset: " << ebx << "\n"; 
+    std::cout << "\tsize: " << eax << "\n"; 
+
+
+
+    // State Component 4 is MPX
+    __cpuIdCount(XSTATE_CPUID, 4,  &eax, &ebx, &ecx, &edx); 
+    std::cout << "State Component 4:  \n"; 
+    std::cout << "\tflag: " << ecx <<  "\n"; 
+    std::cout << "\toffset: " << ebx << "\n"; 
+    std::cout << "\tsize: " << eax << "\n"; 
+
+
+    // State Component 5 is AVX-512
+    __cpuIdCount(XSTATE_CPUID, 5,  &eax, &ebx, &ecx, &edx); 
+    std::cout << "State Component 5:  \n"; 
+    std::cout << "\tflag: " << ecx <<  "\n"; 
+    std::cout << "\toffset: " << ebx << "\n"; 
+    std::cout << "\tsize: " << eax << "\n"; 
+
     return 0; 
 }
